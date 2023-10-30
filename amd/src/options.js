78caf215363f06ce4a97b86f19ca60face1491dc
Janhavi Sonawane <janhavi.sonawane@yuja.com>
2023-10-30 10:50:42 -0400

    [EVP-7701]Feature - Media Chooser - Please update Moodle Media Chooser plugin so that the YuJa Media Chooser is available on under the "Insert" button of the new TinyMCE editor as Kaltura does attached.
    - added new plugin for new tinymce editor

---
+++ amd/src/options.js
@@ -0,0 +1,56 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * YuJa Plugin for Moodle Tiny
+ * @package    tiny_yuja/options
+ * @subpackage yuja
+ * @copyright  2023 YuJa
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import {getPluginOptionName} from 'editor_tiny/options';
+import {pluginName} from './common';
+
+
+const contextIdName = getPluginOptionName(pluginName, 'contextid');
+const fpoptions = getPluginOptionName(pluginName,'paramUrls');
+
+/**
+ * Options registration function.
+ *
+ * @param {tinyMCE} editor
+ */
+export const register = (editor) => {
+    const registerOption = editor.options.register;
+
+    // For each option, register it with the editor.
+    // Valid type are defined in https://www.tiny.cloud/docs/tinymce/6/apis/tinymce.editoroptions/
+    registerOption(contextIdName, {
+        processor: 'number',
+    });
+    registerOption(fpoptions, {
+        processor: 'array',
+    });
+};
+
+/**
+ * Fetch the contextId value for this editor instance.
+ *
+ * @param {tinyMCE} editor The editor instance to fetch the value for
+ * @returns {object} The value of the myFirstProperty option
+ */
+export const getContextId = (editor) => editor.options.get(contextIdName);
+export const getFpoptions = (editor) => editor.options.get(fpoptions);
